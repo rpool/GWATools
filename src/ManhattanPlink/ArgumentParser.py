357cf2d16ea8d9bc491151e39b53d251d8c7a102
r.pool <r.pool@338b351a-9dbe-49a1-b0f1-83e63def89a4>
2012-05-08 14:59:20 +0000

    Finished Manhattan module for plink output. Need to merge this with the snptest version!
    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools/trunk@16 338b351a-9dbe-49a1-b0f1-83e63def89a4

---
+++ src/ManhattanPlink/ArgumentParser.py
@@ -0,0 +1,77 @@
+import argparse
+
+import Logger
+
+def LogArguments(Log=Logger,
+                 ArgParser=argparse.ArgumentParser,
+                 Arguments=argparse.Namespace):
+    ArgParser.print_help()
+    ArgParser.print_help(Log.GetFileHandle())
+
+#   Calculate max. of keylength for formatting
+    MaxLen = 0
+    for Key in vars(Arguments).iterkeys():
+        MaxLen = max(MaxLen,len(Key))
+    LogString =  '\n****\n'+\
+                 'Used arguments:\n'+\
+                 '---------------'
+    print LogString
+    Log.Write(LogString+'\n')
+
+    FormatString = '{:<'+str(MaxLen)+'}'
+    LogString    = ''
+    for Key,Value in vars(Arguments).iteritems():
+        LogString += FormatString.format(Key)+': '+str(Value)+'\n'
+    LogString += '****\n'
+    print LogString
+    Log.Write(LogString+'\n')
+    return
+
+def ParseArguments(Log=None):
+    ArgumentParser = argparse.ArgumentParser(description=\
+                                             'This python module can be used for displaying GWA output data.')
+    ArgumentParser.add_argument('-s',
+                                '--plinkoutputfile',
+                                dest='PlinkOutputFile',
+                                help='FILENAME: Name of output file of the plink run (input)',
+                                metavar='FILENAME')
+    ArgumentParser.add_argument('-i',
+                                '--snpinfofile',
+                                dest='SnpInfoFile',
+                                help='FILENAME: Name of SNP info file (input)',
+                                metavar='FILENAME')
+    ArgumentParser.add_argument('-p',
+                                '--plinkoutputpath',
+                                dest='PlinkOutputPath',
+                                help='PATH: Name of output file path of the plink run (input)',
+                                metavar='PATH')
+    ArgumentParser.add_argument('-e',
+                                '--plinkoutputpreext',
+                                dest='PlinkOutputPreExtStr',
+                                help='STRING: Pre-extension string of the plink output files (input)',
+                                metavar='STRING')
+    ArgumentParser.add_argument('-P',
+                                '--nphenotypes',
+                                dest='NPhe',
+                                type=int,
+                                help='INT: Number of phenotypes (input)',
+                                metavar='INT',
+                                default=163)
+    ArgumentParser.add_argument('-C',
+                                '--nchromosomes',
+                                dest='NChr',
+                                type=int,
+                                help='INT: Number of chromosomes (input)',
+                                metavar='INT',
+                                default=22)
+    ArgumentParser.add_argument('-G',
+                                '--greyscale',
+                                dest='boGreyScale',
+                                help='FLAG: Generate Manhattan plots in greyscale  (input)',
+                                action='store_true',
+                                default=False)
+
+    Arguments = ArgumentParser.parse_args()
+
+    return ArgumentParser,\
+           Arguments
---
+++ src/ManhattanPlink/ManHattan.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python
+import os
+import re
+import colorsys
+import copy
+
+import ArgumentParser
+import Logger
+import DataContainer
+
+def main(ExecutableName):
+
+    ArgParser,\
+    Arguments   = ArgumentParser.ParseArguments()
+
+    Ext = ''
+    Log = Logger.Logger(ExecutableName,
+                        Ext)
+    LogString  = '## START TIMESTAMP\n'
+    LogString += str(Log.GetStartDate())+'\n'
+    LogString += '## END TIMESTAMP'
+    print LogString
+    Log.Write(LogString+'\n')
+    LogString = Log.GetStartLogString()
+    print LogString
+    Log.Write(LogString+'\n')
+    ArgumentParser.LogArguments(Log,
+                                ArgParser,
+                                Arguments)
+
+    if(Arguments.PlinkOutputFile!=None):
+        DCs = DataContainer.DataContainers()
+        DCs.ParsePlinkGWAOutput(Arguments.PlinkOutputFile,
+                                Log)
+        DCs.ParseSnpInfoFile(Arguments.SnpInfoFile,
+                             Log)
+        DCs.PlotManhattan(xname='pos',
+                          yname='pvalue',
+                          Log=Log)
+    else:
+        PlinkOutputFiles = []
+        for File in os.listdir(Arguments.PlinkOutputPath):
+            if((re.search(Arguments.PlinkOutputPreExtStr,File)) and
+               (not re.search('.log',File))):
+                PlinkOutputFiles.append(File)
+#        for p in range(Arguments.NPhe):
+        for p in range(78,79):
+            P     = '_PHE'+str(p+1)+'_'
+            Files = []
+            for c in range(10,11):
+#            for c in range(Arguments.NChr):
+                C = '_CHR'+str(c+1)+'_'
+                for File in PlinkOutputFiles:
+                    if(re.search(P,File) and
+                       re.search(C,File)):
+                        Files.append(File)
+            DCsList = DataContainer.ListDataContainers()
+            DCsList.SetPhenotypeName(re.sub('_','',P))
+
+            HSV_tuples = None
+            RGB_tuples = None
+            if(Arguments.boGreyScale):
+                boGrey     = True
+                GreyHSV    = (0.0,0.0,0.4)
+                BlackHSV   = (0.0,0.0,0.0)
+                HSV_tuples = []
+                for i in range(len(Files)):
+                    if(boGrey):
+                        HSV_tuples.append(GreyHSV)
+                        boGrey = False
+                    else:
+                        HSV_tuples.append(BlackHSV)
+                        boGrey = True
+                RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+            else:
+                HSV_tuples = [(x*1.0/len(Files), 0.75, 0.75) for x in range(len(Files))]
+                RGB_tuples = map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples)
+            boParsedSnpInfo = False
+            for i in range(len(Files)):
+                File = Files[i]
+                DCs  = DataContainer.DataContainers()
+                DCs.ParsePlinkGWAOutput(os.path.join(Arguments.PlinkOutputPath,File),
+                                        Log)
+                if(boParsedSnpInfo):
+                    DCs.SnpChrDict        = copy.deepcopy(DCsList.List[0].SnpChrDict)
+                    DCs.SnpPositionDict   = copy.deepcopy(DCsList.List[0].SnpPositionDict)
+                    DCs.XMinMaxPerChrDict = copy.deepcopy(DCsList.List[0].XMinMaxPerChrDict)
+                else:
+                    DCs.ParseSnpInfoFile(Arguments.SnpInfoFile,
+                                         Log)
+                    boParsedSnpInfo = True
+                DCs.Color = RGB_tuples[i]
+                DCsList.List.append(DCs)
+            DCsList.PlotManhattan(xname='SNP',
+                                  yname='P',
+                                  Log=Log)
+
+    LogString = '**** Done :-)'
+    print LogString
+    Log.Write(LogString+'\n')
+    LogString = Log.GetEndLogString()
+    print LogString
+    Log.Write(LogString+'\n')
+    Log.Close()
+
+    return
+
+if(__name__=='__main__'):
+    ExecutableName = os.path.abspath(__file__).split('/')[-1]
+    main(ExecutableName)

22dc4fcd3ffeec9e71c843b8428b47c4a34cb2de
r.pool <r.pool@338b351a-9dbe-49a1-b0f1-83e63def89a4>
2014-04-03 10:35:23 +0000

    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools/trunk@49 338b351a-9dbe-49a1-b0f1-83e63def89a4

--- Svn2GitSync.sh
+++ Svn2GitSync.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-## # Initialization
-## svn2git svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools --verbose --username r.pool --exclude src/RadialGWA
-## git remote add origin git@github.com:rpool/GWATools.git
-## git push origin master
+INITIALIZE=$1
+SVNREPOBASENAME=$2
+IGNORES=$3
+SVNWORKSPACEPATH=$HOME/workspace/$SVNREPOSVNREPOBASENAME
+TMPGITPATH=$HOME/tmp/git
 
-# Sync
-# git push origin master
-# git svn fetch
-# test
-# svn2git --rebase
+if [ $# -eq 0 ]; then
+    echo Usage:
+    echo $0 "\"init/rebase\" \"svn repo basename\" \"ignores (comma separated!)\""
+fi
 
-exit
+if [ "$INIINITIALIZE"=="init" ];then
+    Arguments="-s svn+ssh://clio.psy.vu.nl/home/r.pool/svn/$SVNREPOBASENAME $SVNREPOBASENAME --username=r.pool"
+    if [ "$IIGNORES"!="" ];then
+	    for i in `echo $IGNORES | sed -e s/","/" "/g`
+	    do
+	        Arguments=$Arguments" --ignore-paths=$i"
+	    done
+	fi
+	cd $TMPGITPATH
+	if [ "$SVNREPOBASENAME"!="" ]; then
+	    rm -rf $SVNREPOBASENAME
+	fi
+    git svn clone -s $Arguments
+#    git svn clone -s svn+ssh://clio.psy.vu.nl/home/r.pool/svn/$SVNSVNREPOBASENAME $SVNSVNREPOBASENAME --ignore-paths=src/RadialGWA --username=r.pool
+    cd $SVNREPOBASENAME
+    git remote add origin git@github.com:rpool/$SVNREPOBASENAME.git
+    git push origin --all
+    git push origin --tags
+    for i in `echo $IGNORES | sed -e s/","/" "/g`
+	do
+	    echo $i >> .git/info/exclude
+	    echo .svn >> .git/info/exclude
+	done
+    rm -rf $SVNWORKSPACEPATH/.git
+    cp -rp .git $SVNWORKSPACEPATH
+fi
+
+if [ "$INIINITIALIZE"=="rebase" ]; then
+    cd $SVNWSVNWORKSPACEPATH
+    git svn rebase
+    git push origin --all
+    git push origin --all
+fi
+
+exit 0

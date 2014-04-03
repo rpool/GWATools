bad4dd5c3ed22e57b25ce278dfda12b9348e308c
r.pool <r.pool@338b351a-9dbe-49a1-b0f1-83e63def89a4>
2014-04-03 06:59:28 +0000

    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools/trunk@47 338b351a-9dbe-49a1-b0f1-83e63def89a4

--- Svn2GitSync.sh
+++ Svn2GitSync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Initialization
-svn2git svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools --verbose --username r.pool --exclude src/RadialGWA
+## # Initialization
+## svn2git svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools --verbose --username r.pool --exclude src/RadialGWA
 ## git remote add origin git@github.com:rpool/GWATools.git
-git push origin master
+## git push origin master
 
 # Sync
 # git push origin master

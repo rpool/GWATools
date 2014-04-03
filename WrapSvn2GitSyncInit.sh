9c9fff2eebe17c842134eaa3ae550c9ffc25d664
r.pool <r.pool@338b351a-9dbe-49a1-b0f1-83e63def89a4>
2014-04-03 13:20:00 +0000

    Wrappers.
    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools/trunk@62 338b351a-9dbe-49a1-b0f1-83e63def89a4

---
+++ WrapSvn2GitSyncInit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./Svn2GitSync.sh init GWATools src/RadialGWA > Svn2GitSync.sh.init.o 2> Svn2GitSync.sh.init.e
+
+exit 0
---
+++ WrapSvn2GitSyncRebase.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./Svn2GitSync.sh rebase GWATools src/RadialGWA > Svn2GitSync.sh.rebase.o 2> Svn2GitSync.sh.rebase.e
+
+exit 0

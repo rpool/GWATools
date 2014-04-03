a250e7e84092c455882caa35b3cb7976787feb44
r.pool <r.pool@338b351a-9dbe-49a1-b0f1-83e63def89a4>
2014-04-03 06:44:58 +0000

    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools/trunk@45 338b351a-9dbe-49a1-b0f1-83e63def89a4

--- Svn2GitSync.sh
+++ Svn2GitSync.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+EXCLUDE=src/RadialGWA
+
 ## # Initialization
-## svn2git svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools --verbose --username r.pool --exclude src/RadialGWA
+## svn2git svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools --verbose --username r.pool --exclude $EXCLUDE
 ## git remote add origin git@github.com:rpool/GWATools.git
 ## git push origin master
 
@@ -9,5 +11,12 @@
 # git push origin master
 git svn fetch
 # svn2git --rebase
+if [ "`grep "$EXCLUDE" .git/info/exclude`" == "" ]; then
+    echo $EXCLUDE >> .git/info/exclude
+fi
+git add `git ls-files --others --exclude-standard`
+git commit
+git push origin master
+
 
 exit

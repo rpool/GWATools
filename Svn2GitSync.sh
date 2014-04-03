955b85d31070c1074ee651a16969588cb3428d9d
r.pool <r.pool@338b351a-9dbe-49a1-b0f1-83e63def89a4>
2014-04-03 11:00:50 +0000

    git-svn-id: svn+ssh://clio.psy.vu.nl/home/r.pool/svn/GWATools/trunk@51 338b351a-9dbe-49a1-b0f1-83e63def89a4

--- Svn2GitSync.sh
+++ Svn2GitSync.sh
@@ -6,12 +6,15 @@ IGNORES=$3
 SVNWORKSPACEPATH=$HOME/workspace/$SVNREPOSVNREPOBASENAME
 TMPGITPATH=$HOME/tmp/git
 
+echo Usage:
+echo $0 "\"init/rebase\" \"svn repo basename\" \"ignores (comma separated!)\""
+
 if [ $# -eq 0 ]; then
-    echo Usage:
-    echo $0 "\"init/rebase\" \"svn repo basename\" \"ignores (comma separated!)\""
+    echo "Wrong number of paramers! => EXITING..."
+    exit
 fi
 
-if [ "$INITIALIZE"=="init" ];then
+if [ "$INITIALIZE" == "init" ];then
     Arguments="-s svn+ssh://clio.psy.vu.nl/home/r.pool/svn/$SVNREPOBASENAME $SVNREPOBASENAME --username=r.pool"
     if [ "$IIGNORES"!="" ];then
 	    for i in `echo $IGNORES | sed -e s/","/" "/g`
@@ -38,7 +41,7 @@ if [ "$INITIALIZE"=="init" ];then
     cp -rp .git $SVNWORKSPACEPATH
 fi
 
-if [ "$INITIALIZE"=="rebase" ]; then
+if [ "$INITIALIZE" == "rebase" ]; then
     cd $SVNWORKSPACEPATH
     git svn rebase
     git push origin --all
